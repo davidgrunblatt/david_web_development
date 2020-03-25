a5a2e1b7eca19b63550ceebab216e4e12277d0c2
David <dpg1919@gmail.com>
2020-03-25 01:35:50 -0700

    added new build file with tweaked work section titles

---
+++ client/build/precache-manifest.a0b38144834b8ec39c4a3187a4b87119.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcd6b0fcf7c137f202df07e377371b7a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "56567a8111f2f94456c3",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "cc1d025ca25bacc3c73a",
+    "url": "/static/css/main.05cc2971.chunk.css"
+  },
+  {
+    "revision": "56567a8111f2f94456c3",
+    "url": "/static/js/2.1694cf13.chunk.js"
+  },
+  {
+    "revision": "cc1d025ca25bacc3c73a",
+    "url": "/static/js/main.3f3e34e1.chunk.js"
+  },
+  {
+    "revision": "010ed06030a4deec06a2",
+    "url": "/static/js/runtime~main.04a735df.js"
+  },
+  {
+    "revision": "5de957e2db9ba0afad515a7f55dbb5ec",
+    "url": "/static/media/about2.5de957e2.png"
+  },
+  {
+    "revision": "b032bcff0dd24a958385ef5245124e5d",
+    "url": "/static/media/blindlyScreen.b032bcff.png"
+  },
+  {
+    "revision": "fb346375eed22f2a5ff6eb14dfe44266",
+    "url": "/static/media/david.fb346375.png"
+  },
+  {
+    "revision": "7ef8d703471bda0dc0119c206c32dfd4",
+    "url": "/static/media/email.7ef8d703.png"
+  },
+  {
+    "revision": "a8275ba7ff2421ea37adaebaf2568a3f",
+    "url": "/static/media/full-stack-development.a8275ba7.jpg"
+  },
+  {
+    "revision": "ecef1fbd08a166a114d99d2f672e81aa",
+    "url": "/static/media/goto.ecef1fbd.png"
+  },
+  {
+    "revision": "6783330d5ac6f069721d6c48982ccc94",
+    "url": "/static/media/javascript.6783330d.png"
+  },
+  {
+    "revision": "8dff7b14021513124f9da74e561cdd0c",
+    "url": "/static/media/jumbo.8dff7b14.jpg"
+  },
+  {
+    "revision": "577993a6885b5e56b39204c39af0f449",
+    "url": "/static/media/logo.577993a6.png"
+  },
+  {
+    "revision": "155b42c91749e8a3abe18b304edc0c78",
+    "url": "/static/media/mern.155b42c9.jpg"
+  },
+  {
+    "revision": "811ba796df18b01cf70c1e4bb48fb857",
+    "url": "/static/media/portfolio2.811ba796.png"
+  },
+  {
+    "revision": "a93af726249c85d11e8d567d47ff334d",
+    "url": "/static/media/responsive-design.a93af726.png"
+  },
+  {
+    "revision": "d0d6665b0da93d45294e1310a08197fd",
+    "url": "/static/media/responsive.d0d6665b.jpg"
+  }
+]);
