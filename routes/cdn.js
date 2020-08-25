0cdb12104ba06d4bd64759fbee01b16979092909
david grunblatt <david@davids-MBP.lan>
2020-08-25 10:53:57 -0700

    tweaked

--- routes/cdn.js
+++ routes/cdn.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.sendFile('./script.js');
+    res.sendFile(__dirname + '/script.js');
 });
 
 module.exports = router;
