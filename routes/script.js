f83f72e63f583143c13c2827e00d95b9e4648144
david grunblatt <david@davids-MBP.lan>
2020-08-25 11:07:06 -0700

    tire inventory

--- routes/script.js
+++ routes/script.js
@@ -1,2 +1,34 @@
 
 const test = 'hello world';
+
+const tires = [
+    {
+        id: 0,
+        name: 'Michelin',
+        size: '20e',
+        inStock: true
+    },
+    {
+        id: 1,
+        name: 'BridgeStone',
+        size: '20e',
+        inStock: true
+    },
+    {
+        id: 2,
+        name: 'Firestone',
+        size: '50e',
+        inStock: false
+    },
+    {
+        id: 2,
+        name: 'Victory',
+        size: '50e',
+        inStock: false
+    }
+];
+
+// have all the data here. Iterate through it in the front end and render some HTML.
+// Make them a simple react app hosted on Heroku in which they can login and add / remove from their inventory.
+// Clients can search for tires and check whether they are in stock or not. 
+// No option to purchase just yet. 
