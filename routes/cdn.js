a1d93877aafffe81633ad3a92875e67cb50bf3af
david grunblatt <david@davids-MBP.lan>
2020-08-25 10:52:12 -0700

    test

--- routes/cdn.js
+++ routes/cdn.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send('Yasss quee');
+    res.sendFile('./script.js');
 });
 
 module.exports = router;
