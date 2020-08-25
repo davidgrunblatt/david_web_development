af71f12e3d9e0c7e052dac67ab774fa257e04ce6
david grunblatt <david@davids-MBP.lan>
2020-08-25 10:48:57 -0700

    testing cdn initial setup

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const mail = require('./routes/mail'); 
+const cdn = require('./routes/cdn'); 
 const PORT = process.env.PORT || 3001;
 const compression = require('compression');
 const helmet = require('helmet');
@@ -10,6 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(compression()); 
 app.use('/api/mail', mail); 
+app.use('/api/cdn', cdn); 
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
